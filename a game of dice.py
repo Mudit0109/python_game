b29eee0ccb695c596916deedb6958ce5294eed06
ServiceX00 <86210966+ServiceX00@users.noreply.github.com>
2023-04-30 23:04:29 +0530

    Create a game of dice.py

---
+++ a game of dice.py
@@ -0,0 +1,10 @@
+
+import random
+min = 1
+max = 6
+roll_again = "yes"
+while roll_again == "yes" or roll_again == "y":
+    print("Rolling the die...")
+    print("The value is....")
+    print(random.randint(min, max))
+    roll_again = input("Roll the dices again?")
